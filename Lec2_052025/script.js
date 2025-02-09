1ec4c8242d3fe29b6ac9e160cc2ef06b3b640f5d
harsh0559.be21@chitkara.edu.in <hj696900@gmail.com>
2025-02-09 19:30:31 +0530

    Portfolio Project

--- Lec2_052025/script.js
+++ Lec2_052025/script.js
@@ -104,27 +104,27 @@ document.addEventListener('scroll', function () {
 
         const scrollPercent = scrollPosition / maxScroll;
 
-        const eclipseNoir = [
-            [8, 8, 8],   // #080808 (True Black)
-            [20, 20, 20], // #141414 (Obsidian)
-            [37, 37, 37], // #252525 (Dark Gray)
-            [59, 59, 59], // #3B3B3B (Graphite Gray)
-            [30, 42, 64], // #1E2A40 (Midnight Blue)
-            [22, 51, 85], // #163355 (Deep Navy)
-            [15, 66, 101],// #0F4265 (Steel Blue)
-            [0, 85, 127]  // #00557F (Cool Teal)
+        const midnightPhantom = [
+            [5, 5, 5],    // #050505 (Pure Black)
+            [18, 18, 18],  // #121212 (Dark Graphite)
+            [35, 35, 35],  // #232323 (Deep Charcoal)
+            [47, 47, 59],  // #2F2F3B (Shadow Indigo)
+            [27, 43, 68],  // #1B2B44 (Steel Sapphire)
+            [16, 44, 60],  // #102C3C (Darkened Teal)
+            [11, 31, 42],  // #0B1F2A (Abyss Blue)
+            [6, 14, 22]    // #060E16 (Final Eclipse)
         ];
 
-        const numColors = eclipseNoir.length - 1;
+        const numColors = midnightPhantom.length - 1;
         const step = 1 / numColors;
         let index = Math.min(Math.floor(scrollPercent / step), numColors - 1);
         let factor = (scrollPercent - index * step) / step;
 
-        let r = Math.round(eclipseNoir[index][0] + (eclipseNoir[index + 1][0] - eclipseNoir[index][0]) * factor);
-        let g = Math.round(eclipseNoir[index][1] + (eclipseNoir[index + 1][1] - eclipseNoir[index][1]) * factor);
-        let b = Math.round(eclipseNoir[index][2] + (eclipseNoir[index + 1][2] - eclipseNoir[index][2]) * factor);
+        let r = Math.round(midnightPhantom[index][0] + (midnightPhantom[index + 1][0] - midnightPhantom[index][0]) * factor);
+        let g = Math.round(midnightPhantom[index][1] + (midnightPhantom[index + 1][1] - midnightPhantom[index][1]) * factor);
+        let b = Math.round(midnightPhantom[index][2] + (midnightPhantom[index + 1][2] - midnightPhantom[index][2]) * factor);
 
-        body.style.background = `linear-gradient(180deg, rgb(${r}, ${g}, ${b}) 0%, rgb(${eclipseNoir[numColors][0]}, ${eclipseNoir[numColors][1]}, ${eclipseNoir[numColors][2]}) 100%)`;
+        body.style.background = `linear-gradient(180deg, rgb(${r}, ${g}, ${b}) 0%, rgb(${midnightPhantom[numColors][0]}, ${midnightPhantom[numColors][1]}, ${midnightPhantom[numColors][2]}) 100%)`;
     });
 });
 
